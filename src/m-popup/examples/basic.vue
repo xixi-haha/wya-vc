4f756ec98f85ab3d66bad1134f0affb06beff4e9
rao <601549452@qq.com>
2018-12-07 15:23:28 +0800

    finished popup

---
+++ src/m-popup/examples/basic.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<button @click="handleShow">click</button>
+	</div>
+</template>
+<script>
+import mPopupAction from '../index';
+
+export default {
+	name: "vc-pop-up-basic",
+	components: {
+	},
+	data() {
+		return {
+			show: false
+		};
+	},
+	computed: {
+
+	},
+	methods: {
+		handleShow() {
+			// this.show = true;
+			mPopupAction('xxxxx', {
+				position: 'top'
+			});
+		}
+	}
+};
+
+</script>
---
+++ src/m-popup/m-popup.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import MPopup from './m-popup.vue';
+import { VcInstance } from '../vc/index';
+
+const Dom = document.body;
+const cName = "MPopup";
+
+export default (html, opts = {}) => {
+	VcInstance.APIS[cName] && VcInstance.APIS[cName].$emit('popup-close');
+	const div = document.createElement('div');
+	let vm;
+	const VueComponent = Vue.extend(MPopup);
+	vm = new VueComponent({
+		el: div,
+		propsData: {
+			...opts,
+			html
+		}
+	});
+	vm.show = true;
+	vm.$on('popup-close', () => {
+		setTimeout(() => {
+			vm.$destroy();
+			Dom.removeChild(vm.$el);
+			delete VcInstance.APIS[cName];
+		}, 300);
+	});
+	VcInstance.APIS[cName] = vm;
+	document.body.appendChild(vm.$el);
+	return vm;
+};
