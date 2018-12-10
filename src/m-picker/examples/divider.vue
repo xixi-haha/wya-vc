d3f1a55bd47a3e6533844641d2962eb1d882132c
rao <601549452@qq.com>
2018-12-10 16:05:53 +0800

    pickerr

---
+++ src/m-picker/examples/divider.vue
@@ -0,0 +1,23 @@
+<template>
+	<div/>
+</template>
+
+<script>
+export default {
+	components: {},
+	props: {},
+	data() {
+		return {
+		};
+	},
+	minxin: [],
+	computed: {},
+	watch: {},
+	mounted() {},
+	methods: {},
+	destoryed() {}
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
